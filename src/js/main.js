0d83fe7bd1d87d601466b8aa58af7eaa07887600
Karol Banat <karol.banat@gmail.com>
2022-10-07 15:15:50 +0200

    Basic email validation

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+const emailInput = document.querySelector('.email-form__input');
+const submitBtn = document.querySelector('.email-form__submit-btn');
+const formMessage = document.querySelector('.email-form__message');
+
+const EMAIL_REGEX =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+submitBtn.addEventListener('click', e => {
+	e.preventDefault();
+	const emailValue = emailInput.value;
+	if (!EMAIL_REGEX.test(emailValue)) {
+		emailInput.classList.add('error');
+		formMessage.innerText = 'Please provide a valid email';
+	} else {
+		emailInput.classList.remove('error');
+		emailInput.value = '';
+		formMessage.innerText = '';
+	}
+});
